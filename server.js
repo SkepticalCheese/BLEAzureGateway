978d1b19fb50cddd69b94c86f2b359341035047e
SkepticalCheese <wpbgomes@gmail.com>
2017-05-29 00:42:04 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+/*
+Web server code
+*/
+
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page 
+app.get('/', function(req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+});
+
+app.listen(8080);
+console.log('listenig on port 8080');
