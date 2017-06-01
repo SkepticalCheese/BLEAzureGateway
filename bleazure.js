c1eba6247cee458441ec665b94c25f7718768660
SkepticalCheese <wpbgomes@gmail.com>
2017-05-31 23:20:35 -0600

    Implemeted edit page

--- bleazure.js
+++ bleazure.js
@@ -12,24 +12,44 @@ class Bleazure {
     constructor () {
         debug ('initializing BleAzure');
         sensors = [
-            { name: 'Bloody Mary', id: '00112233-1', type: 'Door sensor', status: 'Connected' },
-            { name: 'Martini',  id: '00112244-1', type: 'Door sensor', status: 'Connected' },
-            { name: 'Scotch',  id: '00112255-1', type: 'Door sensor', status: 'Connected' }
+            { name: 'Bloody Mary',  id: '00112233-1', type: 'Door sensor', status: 'Connected' },
+            { name: 'Martini',      id: '00112244-1', type: 'Door sensor', status: 'Connected' },
+            { name: 'Scotch',       id: '00112255-1', type: 'Door sensor', status: 'Connected' }
         ];
     }
 
+    // Returns all registered sensors
     getAllSensors () {
         return sensors;
     }
 
-    setSensorName (id, newName) {
-        for (var i=0; i<=sensors.len(); i++) {
+    // Returns position of a sensor in the list above. Returns -1 if not found.
+    getSensorPos (id) {
+        for (var i=0; i<=sensors.length; i++) {
+//            debug ('searching:', sensors[i]);
             if (sensors[i].id == id) {
-                sensors[i].name = newName;
-                return (newName);
+                return i;
             }
         }
-        return;
+        return -1;
+    }
+
+    // Sets user friendly name for a sensor
+    setSensorName (id, newName) {
+        var pos = this.getSensorPos (id);
+        if (pos >= 0) {
+            sensors[pos].name = newName;
+            return newName;
+        }
+    }
+
+    // returns a sensor given its ID
+    getSensorById (id) {
+        var pos = this.getSensorPos (id);
+        if (pos >= 0) {
+//            debug ('returning:', sensors[pos]);
+            return (sensors[pos]);
+        }
     }
 }
 
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ Web server code
 // load the things we need
 var debug = require('debug')('server');
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
 
 var Bleazure = require ('./bleazure.js');
 var bleazure = new Bleazure();
@@ -16,12 +20,34 @@ app.set('view engine', 'ejs');
 
 // index page 
 app.get('/', function(req, res) {
+    sendHome(res);
+});
+
+app.get('/edit/:sensorId', function(req, res) {
+//    debug ('edit', sensor);
+    var sensor = bleazure.getSensorById(req.params.sensorId);
+//    debug ('edit', sensor);
+    res.render('pages/edit', {
+        sensor: sensor
+    });
+});
+
+// This route receives the posted form.
+app.post('/edit/:sensorId', function(req, res){
+    var name = req.body.name;
+    debug ('post name:', name);
+    sendHome(res);
+    bleazure.setSensorName (req.params.sensorId, name);
+});
+
+// Helpers
+function sendHome(res) {
     var sensors = bleazure.getAllSensors();
     
     res.render('pages/index', {
         sensors: sensors
     });
-});
+}
 
 app.listen(8080);
-debug('listenig on port 8080');
+debug('listening on port 8080');
